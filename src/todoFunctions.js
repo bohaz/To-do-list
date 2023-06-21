191958de8f6ea082d8387a2ae727339c548b4501
MSA <crystalbluetech@gmail.com>
2023-06-21 20:54:04 +0100

    Update module import/export to like test file with js todoFuction.js

--- src/todoFunctions.js
+++ src/todoFunctions.js
@@ -1,4 +1,4 @@
-function addTask(tasks, description) {
+export function addTask(tasks, description) {
   const newIndex = tasks.length;
   const newTask = {
     description,
@@ -8,37 +8,28 @@ function addTask(tasks, description) {
   tasks.push(newTask);
 }
 
-function updateIndexes(tasks) {
+export function updateIndexes(tasks) {
   tasks.forEach((task, index) => {
     task.index = index + 1;
   });
 }
 
-function deleteTask(tasks, index) {
+export function deleteTask(tasks, index) {
   tasks.splice(index, 1);
   updateIndexes(tasks);
 }
 
-function editTaskDescription(tasks, index, newDescription) {
+export function editTaskDescription(tasks, index, newDescription) {
   if (index >= 0 && index < tasks.length) {
     tasks[index].description = newDescription;
   }
 }
 
-function saveTasks(tasks) {
+export function saveTasks(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function loadTasksFromLocalStorage() {
+export function loadTasksFromLocalStorage() {
   const storedTasks = localStorage.getItem('tasks');
   return storedTasks ? JSON.parse(storedTasks) : [];
 }
-
-export default {
-  addTask,
-  deleteTask,
-  editTaskDescription,
-  saveTasks,
-  loadTasksFromLocalStorage,
-  updateIndexes
-};
--- tests/addAndRemove.test.js
+++ tests/addAndRemove.test.js
@@ -1,19 +1,18 @@
-let { describe, test, expect } = require('jest-circus');
-let { addTask } = require('../src/todoFunctions').default;
-// const addtask = require('../src/tests');
+import { describe, test, expect } from '@jest/globals';
+import { addTask } from '../src/todoFunctions.js';
 
 describe('addTask', () => {
   test('should add a task to an empty tasks array', () => {
     // Arrange
     const tasks = [];
-    const description = "Task 1";
+    const description = 'Task 1';
 
     // Act
     addTask(tasks, description);
 
     // Assert
     expect(tasks.length).toBe(1);
-    expect(tasks[0].description).toBe("Task 1");
+    expect(tasks[0].description).toBe('Task 1');
     expect(tasks[0].completed).toBe(false);
     expect(tasks[0].index).toBe(1);
   });
@@ -21,16 +20,16 @@ describe('addTask', () => {
   test('should add a task to an existing tasks array', () => {
     // Arrange
     const tasks = [
-      { description: "Task 1", completed: false, index: 1 }
+      { description: 'Task 1', completed: false, index: 1 },
     ];
-    const description = "Task 2";
+    const description = 'Task 2';
 
     // Act
     addTask(tasks, description);
 
     // Assert
     expect(tasks.length).toBe(2);
-    expect(tasks[1].description).toBe("Task 2");
+    expect(tasks[1].description).toBe('Task 2');
     expect(tasks[1].completed).toBe(false);
     expect(tasks[1].index).toBe(2);
   });
